07f2c55dd2ab342784e1f7c5dbcdd4d9d1501f44
jerlendds <jerlendds@osintbuddy.com>
2021-08-26 15:50:16 -0600

    Added database tables

--- backend/app/app/models/user.py
+++ backend/app/app/models/user.py
@@ -1,13 +1,13 @@
-import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, func
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
 if TYPE_CHECKING:
     from .item import Item  # noqa: F401
+    from .project import Project  # noqa: F401
 
 
 class User(Base):
@@ -19,7 +19,7 @@ class User(Base):
     company = Column(String, nullable=True)
     country = Column(String, nullable=False)
     is_superuser = Column(Boolean(), default=False)
-    last_updated = Column(DateTime(), default=datetime.datetime.now)
-    created = Column(DateTime(), default=datetime.datetime.utcnow)
+    last_updated = Column(DateTime(timezone=True), server_default=func.now())
+    created = Column(DateTime(timezone=True), server_default=func.now())
     items = relationship("Item", back_populates="owner")
-    projects = relationship("Project", back_populates="owner")
+    projects = relationship("Project", back_populates="user")
