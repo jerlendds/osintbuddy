aae76409bafeac105ae2aa9618673a905966d8fe
jerlendds <jerlends_0x0@protonmail.com>
2021-09-26 10:37:26 -0600

    Fixed communication between crawler and backend

--- backend/app/app/tests/crud/test_user.py
+++ backend/app/app/tests/crud/test_user.py
@@ -74,7 +74,7 @@ def test_get_user(db: Session) -> None:
     username = random_email()
     user_in = UserCreate(email=username, password=password, is_superuser=True)
     user = crud.user.create(db, obj_in=user_in)
-    user_2 = crud.user.get(db, id=user.id)
+    user_2 = crud.user.count(db, id=user.id)
     assert user_2
     assert user.email == user_2.email
     assert jsonable_encoder(user) == jsonable_encoder(user_2)
@@ -88,7 +88,7 @@ def test_update_user(db: Session) -> None:
     new_password = random_lower_string()
     user_in_update = UserUpdate(password=new_password, is_superuser=True)
     crud.user.update(db, db_obj=user, obj_in=user_in_update)
-    user_2 = crud.user.get(db, id=user.id)
+    user_2 = crud.user.count(db, id=user.id)
     assert user_2
     assert user.email == user_2.email
     assert verify_password(new_password, user_2.hashed_password)
