7da234b676fa58685445bcf2f1f8055771bec0c6
jerlendds <jerlends_0x0@protonmail.com>
2021-09-25 20:27:02 -0600

    Refactored crawler, connected crawler service start to endpoint

--- backend/app/app/api/api_v1/endpoints/search.py
+++ backend/app/app/api/api_v1/endpoints/search.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 from typing import Any, List
 
 from fastapi import APIRouter, Depends, HTTPException
@@ -5,12 +7,12 @@ from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
 from app.api import deps
+from app.worker import start_cse_crawl
 
 router = APIRouter()
 
 
-
-@router.post("/", response_model=schemas.Search)
+@router.post("/", response_model=schemas.SearchMetaData)
 def create_search(
     *,
     db: Session = Depends(deps.get_db),
@@ -18,15 +20,19 @@ def create_search(
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
-    Create new search, authentication required
+    Create new search Google CSE search for all links, authentication required.
+
+    Spin up Scrapy Google CSE spiders that spawn somewhat quickly and begins saving
+    Google CSE results for your query to the database.
     """
-    print("HELLO", search_in, current_user.id)
     search = crud.search.create_with_owner(db=db, obj_in=search_in)
-    print("SEARCH", search)
 
-    user_search_in = schemas.user_search.UserSearchCreate(**{'user_id': current_user.id, 'search_id': search.id})
+    user_search_data = {'user_id': current_user.id, 'search_id': search.id}
+    user_search_in = schemas.user_search.UserSearchCreate(**user_search_data)
     user_search = crud.user_search.create(db=db, obj_in=user_search_in)
-    print("USER_SEARCH", user_search.id, user_search.search_id, user_search.user_id)
-    return search
 
+    crawler_response = start_cse_crawl(search.query, current_user.id, user_search.id)
+    crawler_data = json.loads(crawler_response.get('search_meta'))
+    crawler_data['last_updated'] = datetime.datetime.now()
 
+    return schemas.SearchMetaData(**crawler_data)
