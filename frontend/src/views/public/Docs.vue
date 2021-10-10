95b76e607693613ff9ced7abe7b9db56a490b926
jerlendds <jerlendds@osintbuddy.com>
2021-10-10 07:41:23 -0600

    Added coming soon pages

---
+++ frontend/src/views/public/Docs.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="flex items-center justify-center">
+        <dual-header small-header="Sorry, the docs aren't ready yet" header="Coming Soon" />
+    </section>
+</template>
+
+<script>
+import DualHeader from "@/components/typography/DualHeader";
+
+export default {
+  name: "Docs",
+
+    components: {
+      DualHeader
+    }
+}
+</script>
