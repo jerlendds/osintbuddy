54ffeb1a756f98807a1ebea62263ff8d2051dfb0
jerlendds <jerlendds@proton.me>
2023-02-11 16:05:48 -0700

    feat: adding traceroute transformation

--- frontend/src/routes/osint/_nodes/IpNode.tsx
+++ frontend/src/routes/osint/_nodes/IpNode.tsx
@@ -106,7 +106,7 @@ export default function IpNodeContext({
                   x: rect.x + 160,
                   y: rect.y + 80,
                 }),
-                {...resp.data}
+                { ...resp.data }
               );
               addEdge(parentId, nodeId);
             });
@@ -115,6 +115,30 @@ export default function IpNodeContext({
         >
           <IpIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
           To Geolocation
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={(event) => {
+            const nodeId = `rw${getId()}`;
+            let rect = node.getBoundingClientRect();
+            api.get(`/extract/ip/traceroute?ip=${nodeData[0].value}`).then((resp) => {
+              addNode(
+                nodeId,
+                'traceroute',
+                reactFlowInstance.project({
+                  x: rect.x + 160,
+                  y: rect.y + 80,
+                }),
+                { ...resp.data }
+              );
+              addEdge(parentId, nodeId);
+            });
+          }}
+          className='hover:bg-light-500 hover:text-gray-900 text-gray-700 group flex items-center px-4 py-2 text-sm w-full'
+        >
+          <IpIcon className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+          To Traceroute
         </button>
       </div>
     </div>
