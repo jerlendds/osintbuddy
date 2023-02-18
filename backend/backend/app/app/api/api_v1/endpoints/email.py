3415d99a21fe54fc34ddfdb711f9247b5c4954ff
jerlendds <jerlendds@proton.me>
2023-02-17 18:12:16 -0700

    feat: adding smtp-test for emails

--- backend/backend/app/app/api/api_v1/endpoints/email.py
+++ backend/backend/app/app/api/api_v1/endpoints/email.py
@@ -16,6 +16,9 @@ from sqlalchemy.orm import Session
 from selenium.webdriver import Remote
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support import expected_conditions as EC
 from celery.result import AsyncResult
 
@@ -24,9 +27,14 @@ from app import crud, schemas, models
 from app.api import deps
 from app.core.celery_app import app
 from app.worker import brute_force_subdomains
+from app.core.logger import get_logger
+
+
+logger = get_logger(name=" /extract/email ")
 
 
 router = APIRouter(prefix='/extract/email')
+
 
 
 @router.get('/breaches')
@@ -44,4 +52,20 @@ def have_i_been_pwned(
     except Exception as e:
         print(e)
         return []
-    
+
+
+@router.get('/smtp-test')
+def smtp_test(
+    current_user: models.User = Depends(deps.get_current_active_user),
+    db: Session = Depends(deps.get_db),
+    email: str = "",
+    driver: Remote = Depends(deps.get_driver)
+):
+    try:
+        driver.get("https://centralops.net/co/emaildossier.aspx")
+        xpath = '//*[@id="email"]'
+        driver.find_element(by=By.XPATH, value=xpath).send_keys(email)
+        driver.find_element(by=By.XPATH, value='//*[@id="go"]').click()
+        return driver.find_element(by=By.XPATH, value="/html/body/p[2]").text.split('\n')
+    except Exception as e:
+        logger.error(e)
