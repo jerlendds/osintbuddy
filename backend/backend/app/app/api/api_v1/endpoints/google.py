d1c9dbf0a6e6ee116af06415b55fbef4c2291fb2
jerlendds <jerlendds@proton.me>
2023-02-17 18:11:21 -0700

    style: adding whitespace

--- backend/backend/app/app/api/api_v1/endpoints/google.py
+++ backend/backend/app/app/api/api_v1/endpoints/google.py
@@ -1,11 +1,8 @@
-import urllib
 from typing import Any
-import requests
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import crud, schemas, models
 from app.api import deps
-from app.neomodels.google import GoogleResult, GoogleSearch, get_google_search_results
 from app.core.logger import get_logger
 from app.api.extractors import get_google_results
 
@@ -15,7 +12,7 @@ logger = get_logger(name=" /extract/google ")
 
 router = APIRouter(prefix='/extract/google')
 
- 
+
 @router.get('/search', response_model=Any)
 def get_search_results(
     current_user: models.User = Depends(deps.get_current_active_user),
