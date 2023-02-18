e78f0f34682d36b712fd1f2a7ba8a91b29a4dab2
jerlendds <jerlendds@proton.me>
2023-02-17 18:10:57 -0700

    feat: updating neomodel

--- backend/backend/app/app/neomodels/google.py
+++ backend/backend/app/app/neomodels/google.py
@@ -1,4 +1,4 @@
-from neomodel import StructuredNode, StringProperty, ArrayProperty, RelationshipTo, RelationshipFrom, IntegerProperty
+from neomodel import StructuredNode, StringProperty, ArrayProperty, RelationshipTo, RelationshipFrom, IntegerProperty, DateTimeProperty
 
 
 class GoogleResult(StructuredNode):
@@ -8,10 +8,12 @@ class GoogleResult(StructuredNode):
     url = StringProperty(unique_index=True)
     question = StringProperty()
     result_type = StringProperty()
+    index = IntegerProperty()
     search = RelationshipTo('GoogleSearch', 'GOOGLESEARCH')
 
 
 class GoogleSearch(StructuredNode):
+    last_searched = DateTimeProperty(default_now=True)
     search_query = StringProperty(unique_index=True)
     pages = IntegerProperty()
     related_searches = ArrayProperty(base_property=StringProperty())
@@ -19,14 +21,16 @@ class GoogleSearch(StructuredNode):
     results = RelationshipFrom('GoogleResult', 'GOOGLESEARCH')
 
 
-def get_google_search_results(tx, search_query, pages):
+def get_google_search_results(tx, search_query, pages):     
     return [{
+            "id": result._id,
             "result_type": result.get("result_type"),
+            "index": result.get("index"),
             "breadcrumb": result.get("breadcrumb"),
             "description": result.get("description"),
             "title": result.get("title"),
             "url": url,
-        } for record in tx.run((
+    } for record in tx.run((
             "MATCH (n:GoogleSearch {search_query: $search_query, pages: $pages})"
             "-[]-(r) RETURN n, r"
-        ), search_query=search_query, pages=pages) if (url := (result := record['r']).get('url'))]
+    ), search_query=search_query, pages=pages) if (url := (result := record['r']).get('url'))]
