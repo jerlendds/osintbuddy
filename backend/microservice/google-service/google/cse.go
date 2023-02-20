06dc28a6d8dd083b6277670e3aa6c32de3ccc864
jerlendds <jerlendds@proton.me>
2023-02-20 10:38:52 -0700

    feat: adding google cse searching

--- backend/microservice/google-service/google/cse.go
+++ backend/microservice/google-service/google/cse.go
@@ -13,12 +13,10 @@ import (
 	"time"
 
 	"github.com/gocolly/colly"
-	"github.com/gocolly/colly/queue"
+	// "github.com/gocolly/colly/queue"
 )
 
-var cseResults = new(SerpResults)
-
-func CrawlCse(searchQuery string, pages string, cseUrl string) {
+func CrawlCse(searchQuery string, pages string, cseUrl string)  map[string]interface{} {
 	var paginationIndex = 1
 	// totalPages, err := strconv.Atoi(pages)
 	// if err != nil {
@@ -45,45 +43,47 @@ func CrawlCse(searchQuery string, pages string, cseUrl string) {
 
 	c.SetRequestTimeout(10 * time.Second)
 
-	q, _ := queue.New(
-		2, // Number of consumer threads
-		&queue.InMemoryQueueStorage{MaxSize: 10000}, // Use default queue storage
-	)
+	// q, _ := queue.New(
+	// 	2, // Number of consumer threads
+	// 	&queue.InMemoryQueueStorage{MaxSize: 10000}, // Use default queue storage
+	// )
 
 	c.OnRequest(func(r *colly.Request) {
 		setHeaders(r)
 	})
-
+	var rawData string
+ 	var data map[string]interface{}
 	c.OnResponse(func(r *colly.Response) {
-		x := strings.Split(string(r.Body[:]), "})(")
-		jsonString := strings.Split(x[1], ");")[0]
-		var jsonMap map[string]interface{}
-		json.Unmarshal([]byte(jsonString), &jsonMap)
 
-		fmt.Println(jsonMap)
-		r.Save(fmt.Sprintf("%d.html", paginationIndex))
-		paginationIndex += 1
-		vUrl := fmt.Sprintf("https://cse.google.com/cse/element/v1?"+"rsz=filtered_cse&num=100&hl=en&source=gcsc&gss=.com&cselibv=%s&cx=%s&q=%s&safe=off&cse_tok=%s&sort=&exp=&oq=%s&cseclient=hosted-page-client&callback=1431", jsonMap["cselibVersion"], jsonMap["cx"], searchQuery, jsonMap["cse_token"], searchQuery)
-		q.AddURL(vUrl)
-	})
+		urlResp := strings.Split(string(r.Body[:]), "})(")
+		if len(urlResp) > 1 {
+			jsonString := strings.Split(urlResp[1], ");")[0]
+			var jsonMap map[string]interface{}
+			json.Unmarshal([]byte(jsonString), &jsonMap)
+			paginationIndex += 1
+			vUrl := fmt.Sprintf("https://cse.google.com/cse/element/v1?"+"rsz=filtered_cse&num=100&hl=en&source=gcsc&gss=.com&cselibv=%s&cx=%s&q=%s&safe=off&cse_tok=%s&sort=&exp=&oq=%s&cseclient=hosted-page-client&callback=1431", jsonMap["cselibVersion"], jsonMap["cx"], searchQuery, jsonMap["cse_token"], searchQuery)
+			c.Visit(vUrl)
+		} else {
+			rawData = string(r.Body[:])
+			rawData = strings.Split(rawData, "1431(")[1]
+			rawData = strings.Split(rawData, ");")[0]
+			json.Unmarshal([]byte(rawData), &data)
+		}
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println(r, r.Body)
 	})
 
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
 	})
 	if nextPage == "" {
-
-		q.AddURL(reqUrl)
+		c.Visit(reqUrl)
+	} else {
+		return data
 	}
-
-	q.Run(c)
+	return data
 }
 
-func GoogleCSEService(query string, pages string, cseUrl string) SerpResults {
-	serpResults = new(SerpResults)
-	CrawlCse(query, pages, cseUrl)
-	return *serpResults
+func GoogleCSEService(query string, pages string, cseUrl string)  map[string]interface{} {
+	crawlData := CrawlCse(query, pages, cseUrl)
+	return crawlData
 }
