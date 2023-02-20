df7daf9984e399af5e24523edef3af16234e5689
jerlendds <jerlendds@proton.me>
2023-02-20 11:16:40 -0700

    feat: adding ip to subdomains transformation

--- backend/backend/app/app/api/api_v1/endpoints/ip.py
+++ backend/backend/app/app/api/api_v1/endpoints/ip.py
@@ -154,9 +154,21 @@ def get_subdomains(
         params = {
             'q': ip,
         }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+        }
         response = requests.post(
-            'https://api.hackertarget.com/reverseiplookup/',
-            params=params
+            'https://api.hackertarget.com/reverseiplookup',
+            params=params,
+            headers=headers
         )
         data = response.content.decode('utf8')
         data = data.split('\n')
