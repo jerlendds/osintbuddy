636167d9cd49c3324d202018f0fcfadf504a0c51
jerlendds <jerlendds@osintbuddy.com>
2023-06-13 07:52:11 -0600

    fix: encode query params in cse microservice

--- backend/microservice/google-service/google/cse.go
+++ backend/microservice/google-service/google/cse.go
@@ -1,80 +1,82 @@
 package google
 
 import (
-	// "encoding/json"
 	"encoding/json"
 	"fmt"
 	"log"
-
-	// "net/url"
-	// "strconv"
+	"net/url"
 	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
-	// "github.com/gocolly/colly/queue"
 )
 
 func CrawlCse(searchQuery string, pages string, cseId string) map[string]interface{} {
 	var paginationIndex = 1
-	// totalPages, err := strconv.Atoi(pages)
-	// if err != nil {
-	// panic(err)
-	// }
 	nextPage := ""
-	fmt.Printf("Crawling results for:  %s", cseId)
-	reqUrl := "https://cse.google.com/cse.js?ie=UTF-8&hpg=1&cx=" + cseId
-
-	// Create a new collector
+	cseAuthUrl := "https://cse.google.com/cse.js?ie=UTF-8&hpg=1&cx=" + cseId
 	c := colly.NewCollector()
-	// proxy
-	// rp, err := proxy.RoundRobinProxySwitcher()
-	// if err != nil {
-	// log.Fatal(err)
-	// }
-	// c.SetProxyFunc(rp)
+
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*google.*",
 		Parallelism: 2,
 		RandomDelay: 2 * time.Second,
 	})
-
 	c.SetRequestTimeout(10 * time.Second)
-
-	// q, _ := queue.New(
-	// 	2, // Number of consumer threads
-	// 	&queue.InMemoryQueueStorage{MaxSize: 10000}, // Use default queue storage
-	// )
-
 	c.OnRequest(func(r *colly.Request) {
 		setHeaders(r)
 	})
+
 	var rawData string
 	var data map[string]interface{}
-	c.OnResponse(func(r *colly.Response) {
 
+	c.OnResponse(func(r *colly.Response) {
 		urlResp := strings.Split(string(r.Body[:]), "})(")
 		if len(urlResp) > 1 {
-			jsonString := strings.Split(urlResp[1], ");")[0]
-			var jsonMap map[string]interface{}
-			json.Unmarshal([]byte(jsonString), &jsonMap)
+			fmt.Println("Parsing CSE auth response for token...")
+			cseAuthString := strings.Split(urlResp[1], ");")[0]
+			var authValues map[string]string
+			json.Unmarshal([]byte(cseAuthString), &authValues)
 			paginationIndex += 1
-			vUrl := fmt.Sprintf("https://cse.google.com/cse/element/v1?"+"rsz=filtered_cse&num=100&hl=en&source=gcsc&gss=.com&cselibv=%s&cx=%s&q=%s&safe=off&cse_tok=%s&sort=&exp=&oq=%s&cseclient=hosted-page-client&callback=1431", jsonMap["cselibVersion"], jsonMap["cx"], searchQuery, jsonMap["cse_token"], searchQuery)
-			c.Visit(vUrl)
+
+			var u, err = url.Parse("https://cse.google.com/cse/element/v1")
+			if err != nil {
+				log.Fatal(err)
+			}
+			cseParams := u.Query()
+			cseParams.Set("rsz", "filtered_cse")
+			cseParams.Set("num", "100") // total results returned, can return up to 100
+			cseParams.Set("hl", "en")   // @todo make this configurable on the frontend
+			cseParams.Set("source", "gcsc")
+			cseParams.Set("gss", ".com")
+			cseParams.Set("cselibv", authValues["cselibVersion"])
+			cseParams.Set("cx", authValues["cx"])
+			cseParams.Set("safe", "off")
+			cseParams.Set("q", searchQuery)
+			cseParams.Set("oq", searchQuery)
+			cseParams.Set("cse_tok", authValues["cse_token"])
+			cseParams.Set("gss", ".com")
+			cseParams.Set("callback", "1431")
+			cseParams.Set("sort", "")
+			cseParams.Set("exp", "")
+			cseParams.Set("cseclient", "hosted-page-client")
+			u.RawQuery = cseParams.Encode()
+			fmt.Printf("\nRequesting CSE data: %s ", u.String())
+			c.Visit(u.String())
 		} else {
 			rawData = string(r.Body[:])
 			rawData = strings.Split(rawData, "1431(")[1]
 			rawData = strings.Split(rawData, ");")[0]
 			json.Unmarshal([]byte(rawData), &data)
 		}
-
 	})
 
 	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
+		log.Println("Something went wrong: ", err)
 	})
 	if nextPage == "" {
-		c.Visit(reqUrl)
+		fmt.Printf("CSE Auth Request: %s  \n", cseAuthUrl)
+		c.Visit(cseAuthUrl)
 	} else {
 		return data
 	}
