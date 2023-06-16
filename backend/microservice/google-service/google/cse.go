09052ef039bdd951cf4dff424aab016121d144f1
jerlendds <jerlendds@proton.me>
2023-06-16 08:01:49 -0600

    fix: remove string.Split causing error when results contain ');' + file cleanup

--- backend/microservice/google-service/google/cse.go
+++ backend/microservice/google-service/google/cse.go
@@ -4,38 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/url"
 	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
 )
-
-func getCseDataUrl(searchQuery string, authValues map[string]string) string {
-	var u, err = url.Parse("https://cse.google.com/cse/element/v1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	cseParams := u.Query()
-	cseParams.Set("rsz", "filtered_cse")
-	cseParams.Set("num", "100") // total results returned, can return up to 100
-	cseParams.Set("hl", "en")   // @todo make this configurable on the frontend
-	cseParams.Set("source", "gcsc")
-	cseParams.Set("gss", ".com")
-	cseParams.Set("cselibv", authValues["cselibVersion"])
-	cseParams.Set("cx", authValues["cx"])
-	cseParams.Set("safe", "off")
-	cseParams.Set("q", searchQuery)
-	cseParams.Set("oq", searchQuery)
-	cseParams.Set("cse_tok", authValues["cse_token"])
-	cseParams.Set("gss", ".com")
-	cseParams.Set("callback", "1431")
-	cseParams.Set("sort", "")
-	cseParams.Set("exp", "")
-	cseParams.Set("cseclient", "hosted-page-client")
-	u.RawQuery = cseParams.Encode()
-	return u.String()
-}
 
 func CrawlCse(searchQuery string, pages string, cseId string) map[string]interface{} {
 	var paginationIndex = 1
@@ -71,8 +44,8 @@ func CrawlCse(searchQuery string, pages string, cseId string) map[string]interfa
 			c.Visit(dataUrl)
 		} else {
 			rawData = string(r.Body[:])
-			rawData = strings.Split(rawData, "1431(")[1]
-			rawData = strings.Split(rawData, ");")[0]
+			rawData = strings.Split(rawData, "1984(")[1]
+			rawData = strings.TrimSuffix(rawData, ");")
 			json.Unmarshal([]byte(rawData), &data)
 		}
 	})
@@ -80,11 +53,10 @@ func CrawlCse(searchQuery string, pages string, cseId string) map[string]interfa
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong: ", err)
 	})
+
 	if nextPage == "" {
 		fmt.Printf("CSE Auth Request: %s  \n", cseAuthUrl)
 		c.Visit(cseAuthUrl)
-	} else {
-		return data
 	}
 	return data
 }
