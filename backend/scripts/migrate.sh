0f71297b200ed7f9885ab86e666273e3dc6c2c68
jerlendds <jerlendds@proton.me>
2023-06-20 22:35:55 -0600

    chore: remove unused scripts

--- backend/scripts/migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker compose exec backend alembic revision --autogenerate $1 "$2"
-docker compose exec backend alembic upgrade head
-
-# Fix alembic permissions issue
-docker compose exec backend chmod -R 777 ../app/alembic/versions/
--- backend/scripts/psql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker compose exec db psql --user app -d app
-# SELECT * FROM search INNER JOIN user_search ON search.id = user_search.id INNER JOIN cse_search_result ON cse_search_result.search_id = search.id;
