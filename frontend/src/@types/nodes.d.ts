940739ec7eabd9cc500af53b7cadce2c90cdcc91
jerlendds <jerlendds@proton.me>
2023-06-23 23:21:38 -0600

    chore: cleanup types

--- frontend/src/@types/nodes.d.ts
+++ frontend/src/@types/nodes.d.ts
@@ -1,7 +1,4 @@
-interface FetchProps {
-  pageSize: number;
-  pageIndex: number;
-}
+import { XYPosition } from 'reactflow';
 
 interface NodeMenu {
   id: string;
@@ -10,4 +7,19 @@ interface NodeMenu {
   label: string;
   transforms: string[];
   bounds: DOMRect;
+}
+
+export interface AddNode {
+  id: number;
+  type: string;
+  position: XYPosition;
+  data: any;
+}
+
+export interface AddEdge {
+  source: string;
+  target: string;
+  sourceHandle?: string | undefined;
+  targetHandle?: string | undefined;
+  type?: string | undefined;
 }
