2d5c2bf8e987d649ecbd0791ce8944bac7d8fe85
jerlendds <jerlendds@osintbuddy.com>
2023-09-14 19:29:45 -0600

    fix: route error

---
+++ frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import React, { ErrorInfo } from "react";
+interface IProps {
+  children?: React.ReactElement;
+}
+
+interface IState {
+  hasError: boolean;
+}
+export default class ErrorBoundary extends React.Component<IProps, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
