b5ff48727e9852cb27e4392beb5a036aaac30242
jerlendds <jerlendds@osintbuddy.com>
2023-10-31 12:14:34 -0600

    Rework frontend isActiveTab state to account slice

--- frontend/src/features/account/accountSlice.ts
+++ frontend/src/features/account/accountSlice.ts
@@ -1,42 +1,41 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { RootState } from '@/app/store';
-import { CasdoorUser } from '@/app/openapi';
-import { LS_USER_KEY } from '@/app/api';
-import { useLocalStorage } from '@/components/utils';
+import { LS_USER_KEY, lUserDefault } from '@/app/baseApi';
+import { lStorage } from '@/app/utilities';
+import { useNavigate } from 'react-router-dom';
+
+export type DashboardTab = 'entities' | 'market' | 'graphs'
 
 export interface Account {
   showSidebar: boolean;
   isAuthenticated: boolean;
-  user: CasdoorUser | null;
+  activeDashboardTab: DashboardTab;
 }
 
-let ACCOUNT_INIT = useLocalStorage(LS_USER_KEY)
-if (!ACCOUNT_INIT) ACCOUNT_INIT = useLocalStorage(LS_USER_KEY, { isAuthenticated: false, user: null })
+let ACCOUNT_INIT = lStorage(LS_USER_KEY)
+if (!ACCOUNT_INIT) ACCOUNT_INIT = lStorage(LS_USER_KEY, lUserDefault(false))
 
 const initialState: Account = {
   isAuthenticated: ACCOUNT_INIT?.isAuthenticated,
   showSidebar: true,
-  user: ACCOUNT_INIT?.user
+  activeDashboardTab: 'graphs'
 };
 
 export const account = createSlice({
   name: 'account',
   initialState,
   reducers: {
+    signOut: (state) => {
+      state.isAuthenticated = false;
+      localStorage.removeItem(LS_USER_KEY)
+    },
+    setActiveTab: (state, action: PayloadAction<DashboardTab>) => {
+      state.activeDashboardTab = action.payload
+    },
     setIsAuthenticated: (state, action: PayloadAction<boolean>) => {
       state.isAuthenticated = action.payload
-      if (action.payload === false) {
-        state.user = null
-        localStorage.removeItem(LS_USER_KEY)
-      }
-    },
-    setUser: (state, action: PayloadAction<CasdoorUser | null>) => {
-      state.user = action.payload
-      if (action.payload !== null) {
-        state.isAuthenticated = true
-        useLocalStorage(LS_USER_KEY, { isAuthenticated: true, user: action.payload})
-      }
+      if (action.payload === false) localStorage.removeItem(LS_USER_KEY)
     },
     closeSidebar: (state) => {
       state.showSidebar = false;
@@ -50,11 +49,10 @@ export const account = createSlice({
   },
 });
 
-export const { closeSidebar, openSidebar, setSidebar, setIsAuthenticated, setUser } = account.actions;
+export const { closeSidebar, openSidebar, setSidebar, setIsAuthenticated, signOut, setActiveTab } = account.actions;
 
 export const isSidebarOpen = (state: RootState) => state.account.showSidebar;
-
-export const selectIsAuthenticated = (state: RootState) => state.account.isAuthenticated
-export const selectUser = (state: RootState) => state.account.user
+export const selectActiveTab = (state: RootState) => state.account.activeDashboardTab;
+export const selectIsAuthenticated = (state: RootState) => state.account.isAuthenticated;
 
 export default account.reducer;
