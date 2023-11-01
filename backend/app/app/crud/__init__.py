9d56460b11859405650e98dca5f126b0cd249410
jerlendds <jerlendds@osintbuddy.com>
2023-11-01 07:45:58 -0600

    Polish crud __init__

--- backend/app/app/crud/__init__.py
+++ backend/app/app/crud/__init__.py
@@ -1,5 +1,4 @@
-from app.crud.crud_graphs import graphs  # noqa
-from app.crud.crud_entities import entities  # noqa
-from app.crud.crud_user import user  # noqa
+from app.crud.crud_graphs import graphs
+from app.crud.crud_entities import entities
+from app.crud.crud_user import user
 from app.crud.crud_scan_machines import scan_machine
-    
