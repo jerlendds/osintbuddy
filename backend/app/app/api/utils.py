13576309d558b196459b1636e1b4af17bf2d4ee8
jerlendds <jerlendds@osintbuddy.com>
2023-11-01 07:46:35 -0600

    Update FastAPI casdoor app state type_checking

--- backend/app/app/api/utils.py
+++ backend/app/app/api/utils.py
@@ -2,27 +2,29 @@ from typing import TYPE_CHECKING
 
 from fastapi import Request
 
-# https://github.com/tiangolo/fastapi/issues/632#issuecomment-545574191
+# https://github.com/tiangolo/fastapi/issues/632#issuecomment-545790471
 APIRequest = Request
 if TYPE_CHECKING:
     from fastapi import FastAPI
     from casdoor import CasdoorSDK
+
     class APIServerState:
         CASDOOR_SDK: CasdoorSDK
         REDIRECT_URI: str
         SECRET_TYPE: str
         SECRET_KEY: str
-    def __init__(self) -> None:
-        self.CASDOOR_SDK: CasdoorSDK
-        self.REDIRECT_URI: str
-        self.SECRET_TYPE: str
-        self.SECRET_KEY: str
+        def __init__(self) -> None:
+            self.CASDOOR_SDK: CasdoorSDK
+            self.REDIRECT_URI: str
+            self.SECRET_TYPE: str
+            self.SECRET_KEY: str
+
 
     class APIServer(FastAPI):
         state: APIServerState
+        def setup_state(self) -> None:
+            self.state = APIServerState()
 
-    def setup_state(self) -> None:
-        self.state = APIServerState()
 
     class APIRequest(Request):
         app: APIServer
